8f78af72e65f51860e4a9c5afa2b61c38ce455e4
JakeR1991 <jakerogoff@hotmail.com>
2023-01-03 13:01:08 +0000

    First Commit

---
+++ src/ArrayExample.java
@@ -0,0 +1,8 @@
+public class ArrayExample {
+    public static void main(String[] args) {
+        // use array instead data_type [] varName = { values}
+        String[] car = {"Ford", "BMW", "Ford", "BMW", "Ford", "BMW"}; // six values, index = 0-5
+        // call the element of the array, arrayName [index]
+        System.out.println(car[0]);
+    }
+}
